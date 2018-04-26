ef01286627777e61d42aee4a1f7c5579e444ff93
Trenton Marks <trentmarks111@gmail.com>
2018-04-25 22:33:27 -0600

    all data in orders table rendering successfully to DOM

--- app/models/unapproved.rb
+++ app/models/unapproved.rb
@@ -1,30 +1,41 @@
 class Unapproved
-    # ==================================================
-    #                      SET UP
-    # ==================================================
-    attr_reader :id, :first_name, :last_name
+
+    # attribute readers for instance access
+    attr_reader :id, :driver_id, :restaurant_id, :order_time, :customer_address, :order_subtotal, :payment_type, :tip_type, :dropoff_time, :receipt_image, :submitted_tip, :receipt_approved, :retake_receipt, :no_tip, :cash_tip
 
     # connect to postgres
     DB = PG.connect(host: "localhost", port: 5432, dbname: 'chop_chop')
 
-    def initialize(opts = {})
+    # initialize options hash
+    def initialize opts
         @id = opts["id"].to_i
-        @first_name = opts["first_name"]
-        @last_name = opts["last_name"]
+        @driver_id = opts["driver_id"].to_i
+        @restaurant_id = opts["restaurant_id"].to_i
+        @order_time = opts["order_time"]
+        @customer_address = opts["customer_address"]
+        @order_subtotal = opts["order_subtotal"]
+        @payment_type = opts["payment_type"]
+        @tip_type = opts["tip_type"]
+        @dropoff_time = opts["dropoff_time"]
+        @receipt_image = opts["receipt_image"]
+        @submitted_tip = opts["submitted_tip"]
+        @receipt_approved = opts["receipt_approved"]
+        @retake_receipt = opts["retake_receipt"]
+        @no_tip = opts["no_tip"]
+        @cash_tip = opts["cash_tip"]
     end
 
-    # ==================================================
-    #                      ROUTES
-    # ==================================================
+
+    # Routes
 
     #get all (index)
     def self.all
         results = DB.exec(
             <<-SQL
-                SELECT * FROM drivers;
+                SELECT * FROM orders;
             SQL
         )
-        return results.map { |result| Unapproved.new(result) }
+        return results.map { |result| Unapproved.new result }
     end
 
 end
