2463d975b7c269bce176194ce504db6599f7e9f3
Trenton Marks <trentmarks111@gmail.com>
2018-05-03 14:51:58 -0600

    CHECKPOINT - route /statistics/restaurants/day successfully queries for the correct data and creates an array of objects, each object being the currently null statistics, restaurant id, name, and also each restaurant has an orders property of an array that contains each order and its data that was queried for that restaurants, ordered by id (aka order_time)

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,7 @@
 class Restaurant
 
+    attr_reader :orders
+
     # connect to postgres
     DB = PG.connect(host: "localhost", port: 5432, dbname: 'chop_chop')
 
@@ -53,11 +55,56 @@ class Restaurant
                     })
                 )
             end
-            if result["order_id"]
-                order = Order.new({
-                    "id" => result["order_id"]
+            restaurants.last.orders.push(
+                Order.new({
+                    "id" => result["order_id"],
+                    "ordered_at" => result["ordered_at"],
+                    "assigned_at" => result["assigned_at"],
+                    "restaurant_id" => result["restaurant_id"],
+                    "driver_id" => result["driver_id"],
+                    "customer_phone" => result["customer_phone"],
+                    "customer_location_type" => result["customer_location_type"],
+                    "customer_address" => result["customer_address"],
+                    "customer_city" => result["customer_city"],
+                    "customer_state" => result["customer_state"],
+                    "customer_zip" => result["customer_zip"],
+                    "customer_latitude" => result["customer_latitude"],
+                    "customer_longitude" => result["customer_longitude"],
+                    "order_subtotal" => result["order_subtotal"],
+                    "payment_type" => result["payment_type"],
+                    "tip_type" => result["tip_type"],
+                    "selected_rbt" => result["selected_rbt"],
+                    "actual_rbt" => result["actual_rbt"],
+                    "delivery_notes" => result["delivery_notes"],
+                    "api_eta_at_restaurant" => result["api_eta_at_restaurant"],
+                    "here_restaurant_at" => result["here_restaurant_at"],
+                    "driver_latitude_at_restaurant" => result["driver_latitude_at_restaurant"],
+                    "driver_longitude_at_restaurant" => result["driver_longitude_at_restaurant"],
+                    "not_ready_at" => result["not_ready_at"],
+                    "pickup_at" => result["pickup_at"],
+                    "driver_latitude_at_pickup" => result["driver_latitude_at_pickup"],
+                    "driver_longitude_at_pickup" => result["driver_longitude_at_pickup"],
+                    "api_eta_at_customer" => result["api_eta_at_customer"],
+                    "here_customer_at" => result["here_customer_at"],
+                    "driver_latitude_at_customer" => result["driver_latitude_at_customer"],
+                    "driver_longitude_at_customer" => result["driver_longitude_at_customer"],
+                    "waiting_at" => result["waiting_at"],
+                    "wrong_address_at" => result["wrong_address_at"],
+                    "wrong_phone_at" => result["wrong_phone_at"],
+                    "no_answer_at" => result["no_answer_at"],
+                    "dropoff_at" => result["dropoff_at"],
+                    "driver_latitude_at_dropoff" => result["driver_latitude_at_dropoff"],
+                    "driver_longitude_at_dropoff" => result["driver_longitude_at_dropoff"],
+                    "receipt_image" => result["receipt_image"],
+                    "submitted_tip" => result["submitted_tip"],
+                    "receipt_approved" => result["receipt_approved"],
+                    "retake_receipt" => result["retake_receipt"],
+                    "receipt_approved_by_restaurant" => result["receipt_approved_by_restaurant"],
+                    "retake_receipt_by_restaurant" => result["retake_receipt_by_restaurant"],
+                    "no_tip" => result["no_tip"],
+                    "cash_tip" => result["cash_tip"]
                 })
-            end
+            )
         end
         return restaurants
     end
